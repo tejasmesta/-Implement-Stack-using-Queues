8233a604fcaee604f81238cfb6e880ca02a8a83c
Tejas Mesta <tejasmesta@gmail.com>
2022-08-18 04:59:27 -0700

    Add files via upload

---
+++ Implement Stack using Queues.java
@@ -0,0 +1,71 @@
+class MyStack {
+    
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        
+        mainQ=new ArrayDeque<>();
+        helperQ=new ArrayDeque<>();
+        
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        
+        mainQ.add(x);
+        
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        
+        if(mainQ.size()==0)
+        {
+            return -1;
+        }
+        
+        while(mainQ.size()!=1)
+        {
+            helperQ.add(mainQ.remove());
+        }
+        
+        int val=mainQ.remove();
+   
+        mainQ=helperQ;
+        helperQ=new ArrayDeque<>();
+        
+        return val;
+            
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        
+        if(mainQ.size()==0)
+        {
+            return -1;
+        }
+        
+        while(mainQ.size()!=1)
+        {
+            helperQ.add(mainQ.remove());
+        }
+    
+        int val=mainQ.remove();
+        helperQ.add(val);   //this is the only difference between top and pop.
+    
+        mainQ=helperQ;
+        helperQ=new ArrayDeque<>();
+    
+        return val;
+       
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        
+        return mainQ.isEmpty();
+    }
+}
